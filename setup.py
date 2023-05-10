25e14c8c58a5a0a190d1bf773c12dae8188f027f
Martijn Jacobs <maerteijn@gmail.com>
2023-05-10 14:34:57 +0200

    Dropped Python 3.7 (Django 4+)

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
         "Framework :: Django",
         "Operating System :: Unix",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
