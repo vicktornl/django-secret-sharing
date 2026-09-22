8aeb96ab0e2052064e74163f848d0cb203d2147c
Storm B. Heg <storm@stormbase.digital>
2026-09-22 11:41:35 +0200

    Update minimum support Python / Django

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 install_requires = [
-    "cryptography>=38",
-    "django>=3.2",
+    "cryptography>=50",
+    "django>=5.2",
     "djangorestframework>=3",
 ]
 
@@ -22,7 +22,7 @@ test_requires = [
 
 setup(
     name="django-secret-sharing",
-    version="0.7.0",
+    version="0.8.0",
     description="",
     author="R. Moorman <rob@vicktor.nl>",
     install_requires=install_requires,
@@ -34,12 +34,19 @@ setup(
     zip_safe=False,
     classifiers=[
         "Environment :: Web Environment",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
         "Framework :: Django",
+        "Framework :: Django :: 5.2",
+        "Framework :: Django :: 6.0",
+        "Framework :: Django :: 6.1",
         "Operating System :: Unix",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "Programming Language :: Python :: 3.15",
     ],
 )
